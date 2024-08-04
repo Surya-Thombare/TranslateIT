a9314d6a4ad7d80b3b9791f29728da0389b0765a
Surya-Thombare <suryathombre000@gmail.com>
2024-08-04 12:47:03 +0530

    state

--- app/index.tsx
+++ app/index.tsx
@@ -2,8 +2,18 @@ import { Stack, Link } from 'expo-router';
 import { Text, TextInput, View } from 'react-native';
 import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
 import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
+import { useState } from 'react';
+import { useNavigationState } from '@react-navigation/native';
 
 export default function Home() {
+  const [input, setInput] = useState('')
+  const [output, setOutput] = useState('')
+
+  const onTranslate = () => {
+    const translation = input;
+    setOutput(translation);
+  }
+
   return (
     <>
       <Stack.Screen options={{ title: 'Home' }} />
@@ -17,41 +27,44 @@ export default function Home() {
 
       {/* Inout container */}
       <View className='p-5 border-y border-gray-300'>
-        <View className='flex-row gap-5'> 
+        <View className='flex-row gap-5'>
 
           {/* input */}
-          <TextInput 
-          placeholder='Enter your text'
-          multiline
-          maxLength={5000}
-          className='text-xl flex-1 min-h-32'
+          <TextInput
+            value={input}
+            onChangeText={setInput}
+            placeholder='Enter your text'
+            multiline
+            maxLength={5000}
+            className='text-xl flex-1 min-h-32'
           />
 
           {/* send button */}
-          <FontAwesome6 name="circle-arrow-right" size={24} color="royalblue" />
+          <FontAwesome6 onPress={onTranslate} name="circle-arrow-right" size={24} color="royalblue" />
         </View>
         <View className='flex-row justify-between'>
 
-          {/* mic icon */}          
+          {/* mic icon */}
           <FontAwesome6 name="microphone" size={18} color="dimgray" />
 
           {/* Text count */}
-          <Text className='color-gray-500'>0 / 5000</Text>
+          <Text className='color-gray-500'>{input.length} / 5000</Text>
         </View>
-        
-      </View>
-      {/* OutPut container */}
-      <View className='p-5 gap-5 bg-gray-300'>
-        {/* output */}
-        <Text className='min-h-32 text-xl'>OutPut</Text>
-          <View className='flex-row justify-between'>
-          {/* mic icon */}          
-          <FontAwesome6 name="volume-high" size={18} color="dimgray" />
 
-          {/* Text count */}
-          <FontAwesome5 name="copy" size={18} color="dimgray" />
-        </View>
       </View>
+
+      {
+        output && (
+          <View className='p-5 gap-5 bg-gray-200'>
+            <Text className='min-h-32 text-xl'>{output}</Text>
+            <View className='flex-row justify-between'>
+              <FontAwesome6 name="volume-high" size={18} color="dimgray" />
+              <FontAwesome5 name="copy" size={18} color="dimgray" />
+            </View>
+          </View>
+        )
+      }
+
     </>
   );
 }
